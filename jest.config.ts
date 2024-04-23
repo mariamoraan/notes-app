f6f5b57a47b4a988fd32c4ffadf89123d70eb4ff
mariamoraan <mariamoranluaces@gmail.com>
2024-04-23 19:06:15 +0200

    feat(common): config jest

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  preset: "ts-jest",
+};
+
+export default config;
